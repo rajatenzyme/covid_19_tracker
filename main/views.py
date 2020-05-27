6af7ec35a9090c92cf27061b82b8b35ac52a7192
Rajat Mittal <rajatmittal2012@gmail.com>
2020-05-27 15:48:47 +0530

    first

---
+++ main/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+import requests
+import json
+# Create your views here.
+
+def home(request):
+    url = "https://covid-193.p.rapidapi.com/statistics"
+    querystring = {"country" : "INDIA"}
+
+    headers = {
+        'x-rapidapi-host': "covid-193.p.rapidapi.com",
+        'x-rapidapi-key': "ea75790284mshc149cb256a0fafep18274djsn652e67f52063"
+        }
+
+    response = requests.request("GET", url, headers=headers, params = querystring).json()
+    data = response['response']
+    d = data[0]
+    print(d)
+    context = {
+        'all' : d['cases']['total'],
+        'recovered' : d['cases']['recovered'],
+        'deaths' : d['deaths']['total'],
+        'new' : d['cases']['new'],
+        'critical' : d['cases']['critical'],
+        'tests' : d['tests']['total'],
+        'day' : d['day'],
+        'time' : d['time'],
+        'active' : d['cases']['active'],
+        'new_deaths' : d['deaths']['new']
+    }
+
+
+    return render(request, 'index.html', context)
